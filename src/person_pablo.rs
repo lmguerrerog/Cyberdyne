213e6d0e59f7c6964bec3ca6aec947437df76bc9
pforque <pforque@indra.es>
2022-12-14 12:00:03 +0100

    Los tests de la documentación solo funcionan en lib.rs. Por lo tanto, vamos a hacer uso de tests unitarios.

--- src/person_pablo.rs
+++ src/person_pablo.rs
@@ -6,56 +6,23 @@ pub struct Person {
 
 impl Person {
     /// Esta función crea una persona.
-    ///
-    /// # Ejemplos
-    ///
-    /// ```
-    /// let mut p = Person::new(String::from("John"), 30);
-    /// assert_eq!(p.name, "John");
-    /// assert_eq!(p.age, 30);
-    /// ```
     pub fn new(name: String, age: u8) -> Person {
         Person { name, age }
     }
 
     /// Esta función lee una persona.
-    ///
-    /// # Ejemplos
-    ///
-    /// ```
-    /// let mut p = Person::new(String::from("John"), 30);
-    /// p.read();
-    /// ```
     pub fn read(&self) {
         println!("Name: {}", self.name);
         println!("Age: {}", self.age);
     }
 
     /// Esta función edita una persona.
-    ///
-    /// # Ejemplos
-    ///
-    /// ```
-    /// let mut p = Person::new(String::from("John"), 30);
-    /// p.update(String::from("Jane"), 32);
-    /// assert_eq!(p.name, "Jane");
-    /// assert_eq!(p.age, 32);
-    /// ```
     pub fn update(&mut self, name: String, age: u8) {
         self.name = name;
         self.age = age;
     }
 
     /// Esta función borra una persona.
-    ///
-    /// # Ejemplos
-    ///
-    /// ```
-    /// let mut p = Person::new(String::from("John"), 30);
-    /// p.delete();
-    /// assert_eq!(p.name, "");
-    /// assert_eq!(p.age, 0);
-    /// ```
     pub fn delete(&mut self) {
         self.name = String::new();
         self.age = 0;
@@ -63,23 +30,6 @@ impl Person {
 }
 
 /// Esta función realiza las operaciones CRUD de una persona: crea, edita, lee y borra.
-///
-/// # Ejemplos
-///
-/// ```
-/// let mut p = Person::new(String::from("John"), 30);
-/// p.read();
-/// assert_eq!(p.name, "John");
-/// assert_eq!(p.age, 30);
-/// p.update(String::from("Jane"), 32);
-/// p.read();
-/// assert_eq!(p.name, "Jane");
-/// assert_eq!(p.age, 32);
-/// p.delete();
-/// p.read();
-/// assert_eq!(p.name, "");
-/// assert_eq!(p.age, 0);
-/// ```
 pub fn run() {
     let mut p = Person::new(String::from("John"), 30);
     p.read();
@@ -87,4 +37,38 @@ pub fn run() {
     p.read();
     p.delete();
     p.read();
+}
+
+/// Test para comprobar que los métodos CRUD del objeto Persona funcionan correctamente.
+#[test]
+fn test_main() {
+    // Crear una nueva persona
+    let mut p = Person::new(String::from("John"), 30);
+
+    // Leer los datos de la persona
+    p.read();
+
+    // Comprobar si los datos leídos son los esperados
+    assert_eq!(p.name, "John");
+    assert_eq!(p.age, 30);
+
+    // Actualizar los datos de la persona
+    p.update(String::from("Jane"), 32);
+
+    // Leer los datos actualizados de la persona
+    p.read();
+
+    // Comprobar si los datos leídos son los esperados después de la actualización
+    assert_eq!(p.name, "Jane");
+    assert_eq!(p.age, 32);
+
+    // Eliminar los datos de la persona
+    p.delete();
+
+    // Leer los datos de la persona después de haber sido eliminados
+    p.read();
+
+    // Comprobar si los datos leídos son los esperados después de la eliminación
+    assert_eq!(p.name, "");
+    assert_eq!(p.age, 0);
 }
