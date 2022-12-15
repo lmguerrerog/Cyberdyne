65d9bd42ba9c14ff21b79cc6644340535142ea93
rfleiva <rfleiva@indra.es>
2022-12-15 12:54:50 +0100

    Primer acercamiento a la interfaz y su funcionalidad

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,147 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    println!("test_3");
-    //test
+    //Creación de las variables para dibujar el menú.
+    let linea = "-";
+    let igual = "=";
+    let menu = "| Aplicación entrada datos usuario |";
+    let opcion = "| Selecciona opción a introducir   |";
+
+    // Implementamos la interfaz `Debug` para la estructura `Persona`.
+    struct Persona {
+        nombre: String,
+        apellido1: String,
+        apellido2: String,
+        fecha: String,
+        sexo: String,
+        dni: String,
+    }
+
+    let imprimir_menu = || {
+        println!("{}", igual.repeat(36));
+        println!("{}", menu);
+        println!("{}", igual.repeat(36));
+        println!("{}", opcion);
+        println!("{}", linea.repeat(36));
+        println!("{}", "1. Nombre");
+        println!("{}", "2. Primer apellido");
+        println!("{}", "3. Segundo apellido");
+        println!("{}", "4. Fecha");
+        println!("{}", "5. Sexo");
+        println!("{}", "6. DNI");
+        println!("{}", "7. Imprimir persona");
+        println!("{}", "8. Salir");
+        println!("{}", linea.repeat(33));
+    };
+    struct Datos {
+        persona: Persona,
+    }
+
+    let mut datos = Datos {
+        persona: Persona {
+            nombre: String::new(),
+            apellido1: String::new(),
+            apellido2: String::new(),
+            fecha: String::new(),
+            sexo: String::new(),
+            dni: String::new(),
+        },
+    };
+
+    loop {
+        // Imprimimos el menú.
+        imprimir_menu();
+        // Creamos una persona vacía.
+        let _persona = Persona {
+            nombre: String::new(),
+            apellido1: String::new(),
+            apellido2: String::new(),
+            fecha: String::new(),
+            sexo: String::new(),
+            dni: String::new(),
+        };
+
+        // Leemos la opción del usuario.
+        let mut entrada = String::new();
+        io::stdin()
+            .read_line(&mut entrada)
+            .expect("Fallo en lectura");
+
+        // Convertimos la entrada a un número entero.
+        let entrada: i32 = match entrada.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Realizamos la acción correspondiente a la opción seleccionada.
+        match entrada {
+            1 => {
+                println!("Introduce el nombre");
+                io::stdin()
+                    .read_line(&mut datos.persona.nombre)
+                    .expect("Fallo al leer la línea");
+            }
+            2 => {
+                println!("Introduce el primer apellido");
+                io::stdin()
+                    .read_line(&mut datos.persona.apellido1)
+                    .expect("Fallo al leer la línea");
+            }
+            3 => {
+                println!("Introduce el segundo apellido");
+                io::stdin()
+                    .read_line(&mut datos.persona.apellido2)
+                    .expect("Fallo al leer la línea");
+            }
+            4 => {
+                println!("Introduce la fecha");
+                io::stdin()
+                    .read_line(&mut datos.persona.fecha)
+                    .expect("Fallo al leer la línea");
+            }
+            5 => {
+                println!("Introduce el sexo");
+                io::stdin()
+                    .read_line(&mut datos.persona.sexo)
+                    .expect("Fallo al leer la línea");
+            }
+            6 => {
+                println!("Introduce el DNI");
+                io::stdin()
+                    .read_line(&mut datos.persona.dni)
+                    .expect("Fallo al leer la línea");
+            }
+            7 => {
+                println!("Imprimiendo la persona");
+                if !datos.persona.nombre.is_empty()
+                    && !datos.persona.apellido1.is_empty()
+                    && !datos.persona.apellido2.is_empty()
+                    && !datos.persona.fecha.is_empty()
+                    && !datos.persona.sexo.is_empty()
+                    && !datos.persona.dni.is_empty()
+                {
+                    println!(
+                        "Los datos introducidos son: \t
+                        Nombre: {} \t\t\t\t\t\t Primer apellido: {} \t\t\t\t\t\t Segundo apellido: {} \t
+                        Fecha de nacimiento: {} \t\t\t\t\t\t Sexo: {} \t\t\t\t\t\t DNI: {}",
+                        datos.persona.nombre,
+                        datos.persona.apellido1,
+                        datos.persona.apellido2,
+                        datos.persona.fecha,
+                        datos.persona.sexo,
+                        datos.persona.dni
+                    );
+                } else {
+                    println!("No se han introducido todos los datos");
+                }
+            }
+            8 => {
+                println!("Saliendo del programa");
+                break;
+            }
+            _ => {
+                println!("Opción inválida. Por favor, selecciona una opción del menú.");
+            }
+        }
+    }
 }
